c6b9c80803c0978d8dc847f2da60929b8ce30bc4
Dylan On <dylanon@users.noreply.github.com>
2018-01-25 20:31:31 -0500

    Add link to open up any search

--- src/components/SearchFeed.js
+++ src/components/SearchFeed.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+
 const SearchFeed = props => {
+    // All searches on torontopubliclibrary.ca share this base URL:
+    const searchUrlBase = 'https://www.torontopubliclibrary.ca/search.jsp?Ntt=';
     return (
         <ul className="search-feed">
             {props.searches.map(search => {
-                return <li key={search.receivedAt}>"{search.terms}" ({search.browser})</li>
+                const searchUrlQuery = encodeURIComponent(search.terms);
+                return (
+                    <a href={searchUrlBase + searchUrlQuery} target="_blank">
+                        <li key={search.receivedAt}>"{search.terms}"</li>
+                    </a>
+                )
             })}
         </ul>
     )
