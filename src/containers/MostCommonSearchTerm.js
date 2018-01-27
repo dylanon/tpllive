e56f2c0875cb82b187010e2c2958eb423b70ee97
Dylan On <dylanon@users.noreply.github.com>
2018-01-27 13:55:37 -0500

    Logs an array of most frequent words, but doesn't filter out common prepositions like 'the', 'of', 'a'

--- src/containers/MostCommonSearchTerm.js
+++ src/containers/MostCommonSearchTerm.js
@@ -11,15 +11,49 @@ const mapStateToProps = state => {
 class MostCommonSearchTerm extends Component {
     render() {
         const searches = this.props.searches;
-        // For every search:
-        // Split the search terms by spaces, store terms in array
-        // Count the terms and store the count on an object
-        // Iterate over the object, return an array of the most frequent terms
-        // searches.map
-        searches.forEach(search => {
+        // Create an inventory object
+        const termInventory = searches.reduce((countObject, search) => {
+            // Copy the countObject
+            const nextCountObject = Object.assign({}, countObject);
+            // Split the search terms by spaces, store terms in array
             const terms = search.terms.trim().split(' ');
-            console.log(terms);
+            terms.forEach(term => {
+                const lowerCaseTerm = term.toLowerCase();
+                // Set count to 0 for new terms
+                if (nextCountObject[lowerCaseTerm] === undefined) {
+                    nextCountObject[lowerCaseTerm] = 0;
+                }
+                // Increase the count for the term
+                nextCountObject[lowerCaseTerm] += 1;
+            });
+            // Return an object with terms as property keys, and number of occurences as values
+            return nextCountObject;
+        }, {});
+        // Unpack the inventory object into an array
+        const termArray = [];
+        for (let key in termInventory) {
+            termArray.push({
+                term: key,
+                frequency: termInventory[key]
+            });
+        }
+        // Find the maximum frequency
+        const maxFrequency = termArray.reduce((maxFrequency, term) => {
+            if (term.term !== 'the' && term.term !== 'of') {
+                // Don't count common prepositions: 'the', 'of', 'a'
+                return Math.max(maxFrequency, term.frequency);
+            } else {
+                return maxFrequency;
+            }
+        }, 0);
+        console.log(maxFrequency);
+        // Return an array of items that match the maximum frequency
+        const mostCommonSearchTerms = termArray.filter(term => {
+            // Would also need to filter out common prepositions here 'the', 'of', 'a'
+            return term.frequency === maxFrequency;
         });
+        console.log(mostCommonSearchTerms);
+
         return <TextStat title='Most common search terms' text='dog' />
     }
 }
