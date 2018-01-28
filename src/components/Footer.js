576e42ffcf441cbe67e196aa219978615702eb7f
Dylan On <dylanon@users.noreply.github.com>
2018-01-28 14:17:42 -0500

    Adjust copy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = props => {
     return (
         <footer className="footer">
             <p className="footer__author">An experiment by <a href="https://dylanon.com">Dylan On</a></p>
-            <p className="footer__technologies">React, Redux, JavaScript, WebSockets, HTML5, and CSS3</p>
+            <p className="footer__technologies">Created with React, Redux, JavaScript, WebSockets, HTML5, and CSS3</p>
         </footer>
     )
 }
