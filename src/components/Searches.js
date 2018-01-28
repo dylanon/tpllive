5b6a3cfa78d3fb943b67d93d49756b2f3768d5fd
Dylan On <dylanon@users.noreply.github.com>
2018-01-28 14:07:12 -0500

    Adjust copy

--- src/components/Searches.js
+++ src/components/Searches.js
@@ -6,7 +6,7 @@ const Searches = props => {
     return (
         <section className="searches">
             <h2>Real-time searches</h2>
-            <p>Click a search to see its results on <a href="https://www.tpl.ca" target="_blank" rel="noopener noreferrer">tpl.ca</a>!</p>
+            <p>Click a search to explore results on <a href="https://www.tpl.ca" target="_blank" rel="noopener noreferrer">tpl.ca</a>!</p>
             <SearchFeedListener />
         </section>
     )
