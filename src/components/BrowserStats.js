61980c1e51cdd3749e3aac1df72d21e4ab566453
Dylan On <dylanon@users.noreply.github.com>
2018-01-28 14:07:06 -0500

    Remove seconds

--- src/components/BrowserStats.js
+++ src/components/BrowserStats.js
@@ -8,7 +8,7 @@ const BrowserStats = props => {
     return (
         <section className="browser-stats">
             <h2>Searches by browser</h2>
-            <p>Showing browser stats collected from {now.format('MMMM Do YYYY')} at {now.format('h:mm:ss a')}.</p>
+            <p>Showing browser stats collected from {now.format('MMMM Do YYYY')} at {now.format('h:mm a')}.</p>
             <ul className="browser-list">
                 <PercentageCalculator />
             </ul>
