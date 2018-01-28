aad682cb4dc8569dd3d7071659d517e562b78547
Dylan On <dylanon@users.noreply.github.com>
2018-01-28 13:27:35 -0500

    Add `rel="noopener noreferrer"` to links that open in a new tab/window

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,8 @@ import './About.css';
 const About = props => {
     return (
         <section className="about">
-            <p>Live feed of searches and browser stats from the <a href="https://www.tpl.ca" target="_blank">Toronto Public Library</a> website.</p>
-            <p>Data provided by <a href="https://opendata.tplcs.ca/" target="_blank">Toronto Public Library Open Data</a>. Built by <a href="https://dylanon.com">Dylan On</a>.</p>
+            <p>Live feed of searches and browser stats from the <a href="https://www.tpl.ca" target="_blank" rel="noopener noreferrer">Toronto Public Library</a> website.</p>
+            <p>Data provided by <a href="https://opendata.tplcs.ca/" target="_blank" rel="noopener noreferrer">Toronto Public Library Open Data</a>. Built by <a href="https://dylanon.com">Dylan On</a>.</p>
         </section>
     )
 }
