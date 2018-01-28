4e11291fcaa20cf505ae49c5c422e4c56567ef9b
Dylan On <dylanon@users.noreply.github.com>
2018-01-28 12:06:37 -0500

    Style the About component

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = props => {
     return (
         <section className="about">
             <p>Live feed of searches and browser stats from the <a href="https://www.tpl.ca" target="_blank">Toronto Public Library</a> website.</p>
-            <p>Data provided by <a href="https://opendata.tplcs.ca/" target="_blank">Toronto Public Library Open Data</a>.</p>
+            <p>Data provided by <a href="https://opendata.tplcs.ca/" target="_blank">Toronto Public Library Open Data</a>. Built by <a href="https://dylanon.com">Dylan On</a>.</p>
         </section>
     )
 }
